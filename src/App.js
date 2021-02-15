992938587e484517ea8d3efb394347c7eab4bd02
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2021-02-15 16:08:37 -0500

    scrolldown component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Navbar from "./Components/Navbar/navbar"
+import MyCarousel from './Components/carousel/mycarousel';
+import TitleMessage from "./Components/title-message/title-message"
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MyCarousel/>
+      <TitleMessage/>
     </div>
   );
 }
