ec603193bb1113160fb487ea86644191dcde7d70
Smita Deshpande <smitadeshpande@Smitas-MacBook-Pro.local>
2022-05-16 15:19:17 -0400

    added new experience

--- src/Pages/Experience/experience.js
+++ src/Pages/Experience/experience.js
@@ -19,24 +19,26 @@ const Experience = () => {
                         <Card.Body className="d-flex justify-content-center flex-column">
 
                             <Card.Text className="text-left style">
-                                <strong className="body-title-style ">  TechSmite India Pvt LTD</strong>
+                                <strong className="body-title-style ">  Skitter TV Atlanta GA</strong>
 
 
                             </Card.Text>
 
                             <div>
                                 <Card.Text className="text-left style">
-                                    <strong className="body-title-style ">Junior React-Native Developer</strong>
+                                    <strong className="body-title-style ">Front End  Developer</strong>
                                     <br />
-                                    <strong>Technology:</strong> React JS,React Native,Firebase,HTML,CSS,Javascript,Node JS
+                                    <strong>Technology:</strong> React JS,React Native,XCode,Android Studio,Javascript,Node JS
                                     <br />
-                                    <strong>Duration:</strong> August 2020 - Present
+                                    <strong>Duration:</strong> Feburary 2022 - Present
                                      <br />
                                     <strong> Description </strong>
                                     <ul className="text-left">
                                         <li>
-                                            Working in startup environment on React and React-Native project, Responsible for creating efficient design and developing User Interaction screens for Car Parking Mobile App using React Native. </li>
-                                        <li>Work on Both React Native Expo and React Native cli</li>
+                                        	Skitter TV is premier Application Based IPTV technology solution providing advance Pay TV services for broadband services.
+<li>	Implemented a variety of new features including writing native java modules to handle the interaction of the app with the android operating system or designing new components and logic in react-native utilizing Redux for state management.</li>
+ </li>
+                                      
 
                                         {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
 
@@ -46,6 +48,35 @@ const Experience = () => {
 
                                 </Card.Text>
                             </div>
+
+                            <div>
+                                <Card.Text className="text-left style">
+                                    <strong className="body-title-style ">Benetech - Palo Alto CA </strong>
+                                    <br />
+                                    <strong>Technology:</strong> React JS,React Native,XCode,Android Studio,Javascript,Node JS
+                                    <br />
+                                    <strong>Duration:</strong> Feburary 2022 - Auguest 2022
+                                     <br />
+                                    <strong> Description </strong>
+                                    <ul className="text-left">
+                                        <li>
+                                       <li> 	Working on developing mobile app for BookShare reader for Benetech</li>
+<li>		Creating multiple mobile screen UI using Nativebase library for React Native.
+•	Created navigation and Tab interface for the whole app using React Navigation
+•	Working on Epubjs and Text to speech React native libraries for rendering epub book and make them speak in Android and IOS devices.
+.</li>
+ </li>
+                                      
+
+                                        {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
+
+
+                                    </ul>
+
+
+                                </Card.Text>
+                            </div>
+
 
 
                             <div>
