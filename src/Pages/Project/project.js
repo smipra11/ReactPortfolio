ddec7969ebe5379a6cba54a24f07cf6f5f2c8deb
Smita Deshpande <smitadeshpande@smitas-mbp.mynetworksettings.com>
2024-03-29 17:46:26 -0400

    styling changes

--- src/Pages/Project/project.js
+++ src/Pages/Project/project.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Container } from "react-bootstrap";
 
 import Image from "react-bootstrap/Image";
 import bookshare1 from "../../assets/img/projects/bookshare1.png";
@@ -27,8 +27,11 @@ const Project = () => {
     <div>
       <h3 className="pt-3 text-center  pb-3">PROJECT</h3>
 
-      <Row className="align-items-start p-2  rounded">
-        <Col xs={6} md={6} lg={6}>
+      <Row className="justify-content-center p-2  rounded">
+        <Col className="justify-content-center" lg={6}>
+          <Image src={booksharelogo} alt="logo" className="booksharelogo" />
+        </Col>
+        <Col lg={6} className="justify-content-center ">
           Bookshare Reader is the world's largest library of ebooks for people
           with reading barriers like dyslexia, blindness, low vision, and
           physical disabilities. Listen to books in high quality audio and
@@ -49,51 +52,57 @@ const Project = () => {
             </Button>
           </Row>
         </Col>
-        <Col xs={6} md={6} lg={6}>
-          <Image src={booksharelogo} alt="logo" className="booksharelogo" />
-        </Col>
       </Row>
+      <Container>
+        <Row className="justify-content-center pt-5  ">
+          <Col className="align-items-center">
+            <Row className="justify-content-center">
+            <Image src={bookshare1} className="phoneimage" />
+            </Row>
 
-      <Row className="justify-content-center pt-5  rounded">
-        <Col xs={6} md={6} lg={3}>
-          <Image src={bookshare1} className="phoneimage" />
+            <p className="header text-center">Autheticate User</p>
 
-          <p className="header">Autheticate User</p>
+            <p className="text-center"> 
+              Initial Screen of the app where user can log in by valid username
+              and password
+            </p>
+          </Col>
 
-          <p>
-            Initial Screen of the app where user can log in by valid username
-            and password
-          </p>
-        </Col>
+          <Col>
+          <Row className="justify-content-center">
+            <Image src={bookshare3} className="phoneimage" />
+            </Row>
 
-        <Col xs={6} md={6} lg={3}>
-          <Image src={bookshare3} className="phoneimage" />
+            <p className="header text-center" > Book Search</p>
 
-          <p className="header"> Book Search</p>
+            <p className="text-center">Search the book by Most Recent,Title or Author</p>
+          </Col>
+          <Col>
+          <Row className="justify-content-center">
+            <Image src={bookshare4} className="phoneimage" />
+            </Row>
 
-          <p>Search the book by Most Recent,Title or Author</p>
-        </Col>
-        <Col xs={6} md={6} lg={3}>
-          <Image src={bookshare4} className="phoneimage" />
+            <p className="header text-center"> Audio TextBook</p>
 
-          <p className="header"> Audio TextBook</p>
+            <p className="text-center">
+              Provide all the book information on this screen,user with reading
+              disability can able to read that Audio Textbook
+            </p>
+          </Col>
+          <Col>
+          <Row className="justify-content-center">
+            <Image src={bookshare5} className="phoneimage" />
+            </Row>
 
-          <p>
-            Provide all the book information on this screen,user with reading
-            disability can able to read that Audio Textbook
-          </p>
-        </Col>
-        <Col xs={6} md={6} lg={3}>
-          <Image src={bookshare5} className="phoneimage" />
+            <p className="header text-center" > Book Settings</p>
 
-          <p className="header"> Book Settings</p>
-
-          <p>
-            User with reading disability can change the book setting like font
-            size,line spacing,text color
-          </p>
-        </Col>
-      </Row>
+            <p className="text-center">
+              User with reading disability can change the book setting like font
+              size,line spacing,text color
+            </p>
+          </Col>
+        </Row>
+      </Container>
       <hr
         style={{
           backgroundColor: "lightgrey",
@@ -101,8 +110,12 @@ const Project = () => {
           border: "none",
         }}
       />
-      <Row className="align-items-start pt-5 rounded">
-        <Col xs={6} md={6} lg={6}>
+      <Row className="justify-content-center pt-5 rounded">
+        <Col className="justify-content-center" lg={6}>
+          <Image src={epiclogo} alt="logo" className="booksharelogo" />
+        </Col>
+
+        <Col lg={6}>
           Skitter TV is a leading Application Based IPTV technology solution
           that offers advanced Pay TV services for broadband services. Added
           several new features and designed new react-native components and
@@ -121,49 +134,54 @@ const Project = () => {
             </Button>
           </Row>
         </Col>
-        <Col xs={6} md={6} lg={6}>
-          <Image src={epiclogo} alt="logo" className="booksharelogo" />
-        </Col>
       </Row>
 
       <Row className="justify-content-center pt-5 rounded">
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={epic1} className="epictvimage" />
+          </Row>
 
-          <p className="header">Autheticate User Screen</p>
+          <p className="header text-center">Autheticate User Screen</p>
 
-          <p>
+          <p className="text-center">
             Initial Screen of the app where user can log in by valid username
             and password
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={epic2} className="epictvimage" />
+          </Row>
 
-          <p className="header"> User Recorded Programs</p>
+          <p className="header text-center"> User Recorded Programs</p>
 
-          <p>
+          <p className="text-center">
             User can record programs as series or individual recorded those will
             be displayed on home screen as cards
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={epic3} className="epictvimage" />
+          </Row>
 
-          <p className="header"> Channels Guide Screen</p>
+          <p className="header text-center"> Channels Guide Screen</p>
 
-          <p>
+          <p className="text-center">
             Guide View where individual channels and their episodes along with
             airing time will be diplay.User can switch from mini player to full
             screen view
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={epic4} className="epictvimage" />
+          </Row>
 
-          <p className="header">Previously Viewed Channels</p>
+          <p className="header text-center">Previously Viewed Channels</p>
 
-          <p> Five Previously viewed channels will be diplay on Home screen </p>
+          <p className="text-center"> Five Previously viewed channels will be diplay on Home screen </p>
         </Col>
       </Row>
       <hr
@@ -173,8 +191,11 @@ const Project = () => {
           border: "none",
         }}
       />
-      <Row className="align-items-start pt-5 mt-4 rounded">
-        <Col xs={6} md={6} lg={6}>
+      <Row className="justify-content-center pt-5 mt-4 rounded">
+        <Col className="justify-content-center" lg={6}>
+          <Image src={KerbletLogo} alt="logo" className="booksharelogo" />
+        </Col>
+        <Col lg={6}>
           Kerblet provide multiple Car Parking solutions to customers across
           India. Technology-powered Kerblet Parking platform is changing how
           customers find and pay for parking. Designed and developed user
@@ -194,49 +215,51 @@ const Project = () => {
             </Button>
           </Row>
         </Col>
-        <Col xs={6} md={6}>
-          <Image src={KerbletLogo} alt="logo" className="booksharelogo" />
-        </Col>
       </Row>
 
       <Row className="justify-content-center pt-5 rounded">
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={kb1} className="phoneimage" />
+          </Row>
 
-          <p className="header">Vehicle Details</p>
+          <p className="header text-center">Vehicle Details</p>
 
-          <p>
-            {" "}
+          <p className="text-center">
             Vehicle details allow users to attach multiple vehicles to a single
             account. It boasts a user-friendly interface with autocomplete text
             inputs for vehicle details, an image upload function that verifies
             number plate visibility
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={kb2} className="phoneimage" />
+          </Row>
 
-          <p className="header">Payment History</p>
-          <p>
-            {" "}
+          <p className="header text-center">Payment History</p>
+          <p className="text-center">
             This screen provides a clear and detailed Payment History for a
             parking app, allowing users to track their parking expenses for both
             personal and business needs.
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={kb3} className="phoneimage" />
-          <p className="header">Reserve Parking</p>
-          <p>
-            {" "}
+          </Row>
+          <p className="header text-center">Reserve Parking</p>
+          <p className="text-center">
             This screen offers an intuitive booking process for two-wheelers,
-            with options for timing, duration, and vehicle type.{" "}
+            with options for timing, duration, and vehicle type.
           </p>
         </Col>
-        <Col xs={6} md={6} lg={3}>
+        <Col >
+        <Row className="justify-content-center">
           <Image src={kb4} className="phoneimage" />
-          <p className="header">Parking Map</p>
-          <p>
+          </Row>
+          <p className="header text-center">Parking Map</p>
+          <p className="text-center">
             This screen shows map based interface Users can interact with the
             map to locate and reserve parking. The screen also includes options
             to 'Check In' or 'Check Out', streamlining the user's parking
